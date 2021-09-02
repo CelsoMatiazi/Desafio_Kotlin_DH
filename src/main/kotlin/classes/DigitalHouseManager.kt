ca01c699ad2470814f93aaa07d42ba65d32a48c7
CelsoMatiazi <celsomatiazi@gmail.com>
2021-09-02 15:09:27 -0300

    classe DigitalHouseManager

---
+++ src/main/kotlin/classes/DigitalHouseManager.kt
@@ -0,0 +1,11 @@
+package classes
+
+class DigitalHouseManager(
+    val professores: List<Professor>,
+    val alunos: List<Aluno>,
+    val cursos: List<Curso>,
+    val matriculas: List<Matricula>
+) {
+
+
+}
